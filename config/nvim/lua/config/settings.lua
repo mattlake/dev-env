252d08be800666ac7fce09726ce1777e6fdb43ca
Matthew Lake <matthew.lake@onecom.co.uk>
2026-04-20 09:42:03 +0100

    [zshrc-symlink] clean up nvim config: remove spurious setqflist, redundant keybind, and noise comments

--- config/nvim/lua/config/settings.lua
+++ config/nvim/lua/config/settings.lua
@@ -2,33 +2,25 @@ vim.g.have_nerd_font = true
 vim.opt.title = true
 vim.opt.titlestring = [[Nvim: %t]]
 
--- Show relative line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 999
 
--- share the clipboard
 vim.schedule(function()
 	vim.opt.clipboard = "unnamedplus"
 end)
 
 vim.opt.breakindent = true
-vim.opt.undofile = true -- stores undo history betwwen sessions
+vim.opt.undofile = true
 
 -- make searches case-insensitive unless there is an uppercase letter included
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- enable signcolumn
 vim.opt.signcolumn = "yes"
-
--- Decrease update time
 vim.opt.updatetime = 250
-
--- Decrease mapped sequence wait time
 vim.opt.timeoutlen = 300
 
--- Configure how new splits should be opened
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
@@ -38,10 +30,8 @@ vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 -- Preview substitutions live with :%s
 vim.opt.inccommand = "split"
 
--- which line am i on?
 vim.opt.cursorline = true
 
--- highlight yanks
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking text",
 	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
@@ -53,18 +43,15 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 -- Hide the status line as we are using mini-statusline
 vim.opt.showmode = false
 
--- Tab and indentation settings
-vim.opt.expandtab = true -- Converts tabs to spaces
-vim.opt.shiftwidth = 4 -- indent depth
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.smarttab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 
-vim.opt.mouse = "a" -- Enable the mouse for rezing windows etc
-
-vim.diagnostic.setqflist()
+vim.opt.mouse = "a"
 
 vim.lsp.enable("omnisharp")
 vim.lsp.enable("vtsls")
--- config/nvim/lua/plugins/conform-nvim.lua
+++ config/nvim/lua/plugins/conform-nvim.lua
@@ -4,7 +4,6 @@ return {
 	cmd = { "ConformInfo" },
 	keys = {
 		{
-			-- Customize or remove this keymap to your liking
 			"<leader>f",
 			function()
 				require("conform").format({ async = true })
@@ -13,11 +12,9 @@ return {
 			desc = "Format buffer",
 		},
 	},
-	-- This will provide type hinting with LuaLS
 	---@module "conform"
 	---@type conform.setupOpts
 	opts = {
-		-- Define your formatters
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "isort", "black" },
@@ -26,13 +23,10 @@ return {
 			html = { "prettierd" },
 			cs = { lsp_format = "prefer" },
 		},
-		-- Set default options
 		default_format_opts = {
 			lsp_format = "fallback",
 		},
-		-- Set up format-on-save
 		format_on_save = { timeout_ms = 500 },
-		-- Customize formatters
 		formatters = {
 			shfmt = {
 				append_args = { "-i", "2" },
@@ -40,7 +34,6 @@ return {
 		},
 	},
 	init = function()
-		-- If you want the formatexpr, here is the place to set it
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
 }
